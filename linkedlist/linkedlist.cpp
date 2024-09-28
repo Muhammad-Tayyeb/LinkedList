59801f4579648438043f64a1ff9b7365764656f2
Muhammad-Tayyeb <mtayyebahmadkhan@gmail.com>
2024-09-28 12:03:41 -0700

    descending order.

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -1,6 +1,29 @@
+//in asscending and descending order.
 #include<iostream>
 using namespace std;
 int main()
 {
-    cout<<"ALLAH";
-}
+    int arr[5]={6,3,8,9,2};
+    const int size= 5;
+    int temp;
+
+    for(int i=0; i<size-1; i++)
+    {
+        for (int j=i+1; j<size; j++)
+        {
+            if(arr[i]<arr[j])//used for descending order.
+            // if(arr[i]>arr[j])used for asscending order.
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+    for (int i=0; i<5;i++)
+    {
+        cout <<arr[i]<<"  ";
+    }
+    cout <<endl;
+} 
